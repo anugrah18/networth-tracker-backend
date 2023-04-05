50818047f7f72e93e1c9d5be97eb320686a89d9c
anugrah18 <anugrah18@gmail>
2023-04-04 23:33:30 -0700

    Removed extraneous condition in record handler

--- handlers/RecordHandler.js
+++ handlers/RecordHandler.js
@@ -5,11 +5,6 @@ const responseWithStatus = require("../utils/responseTemplate");
 //Get all records.
 const getAllRecordsHandler = expressAsyncHandler(async (req, res) => {
   try {
-    //If user does not match logged in user or is not an admin.
-    if (!req.user) {
-      return responseWithStatus(res, "Not authorized", 401);
-    }
-
     const userID = req.user.userId;
 
     //Join Record and Item Type
