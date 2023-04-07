cc4a24cc69c6b993a42eb813567783bbb8849245
anugrah18 <anugrah18@gmail>
2023-04-06 20:42:17 -0700

    Added create record route

--- handlers/RecordHandler.js
+++ handlers/RecordHandler.js
@@ -1,5 +1,6 @@
 const expressAsyncHandler = require("express-async-handler");
 const { Record, ItemType } = require("../models/ModelsDefine");
+const { itemTypeId } = require("../models/Schemas/ItemTypeSchema");
 const responseWithStatus = require("../utils/responseTemplate");
 
 //Get all records.
@@ -29,4 +30,29 @@ const getAllRecordsHandler = expressAsyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { getAllRecordsHandler };
+//Create one record
+const createRecordHandler = expressAsyncHandler(async (req, res) => {
+  try {
+    const { recordDate, itemDescription, itemValue, itemTypeId } = req.body;
+
+    const userID = req.user.userId;
+
+    await Record.create({
+      recordDate: recordDate,
+      itemDescription: itemDescription,
+      itemValue: itemValue,
+      itemTypeId: itemTypeId,
+      userId: userID,
+    });
+
+    return responseWithStatus(res, "Record successfully created");
+  } catch (error) {
+    return responseWithStatus(
+      res,
+      "Failed to create a record please check your request",
+      400
+    );
+  }
+});
+
+module.exports = { getAllRecordsHandler, createRecordHandler };
--- routes/Record.js
+++ routes/Record.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { getAllRecordsHandler } = require("../handlers/RecordHandler");
+const {
+  getAllRecordsHandler,
+  createRecordHandler,
+} = require("../handlers/RecordHandler");
 const authMiddleware = require("../middlewares/auth/authMiddleware");
 const recordRoute = express.Router();
 
 recordRoute.get("/", authMiddleware, getAllRecordsHandler);
+recordRoute.post("/", authMiddleware, createRecordHandler);
 
 module.exports = recordRoute;
