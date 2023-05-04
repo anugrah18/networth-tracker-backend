fa73b6092918eeb4ae760f6bc36e0119988263ee
anugrah18 <anugrah18@gmail>
2023-05-03 22:58:02 -0700

    Added inflation bls API

--- routes/Record.js
+++ routes/Record.js
@@ -4,6 +4,7 @@ const {
   createRecordHandler,
   deleteRecordHandler,
   updateRecordHandler,
+  getInflationHandler
 } = require("../handlers/RecordHandler");
 const authMiddleware = require("../middlewares/auth/authMiddleware");
 const recordRoute = express.Router();
@@ -12,5 +13,8 @@ recordRoute.get("/", authMiddleware, getAllRecordsHandler);
 recordRoute.post("/", authMiddleware, createRecordHandler);
 recordRoute.delete("/:id", authMiddleware, deleteRecordHandler);
 recordRoute.put("/:id", authMiddleware, updateRecordHandler);
+
+//inflation data
+recordRoute.post("/inflationData",authMiddleware,getInflationHandler)
 
 module.exports = recordRoute;
